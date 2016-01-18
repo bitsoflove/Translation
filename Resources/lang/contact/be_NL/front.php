34482d8ef302e71fe94d32f9b8e46ab9408faef8
RienMoeyaert <rien@bitsoflove.be>
2016-01-18 10:26:58 +0100

    Added contact lang files

---
+++ Resources/lang/contact/be_NL/front.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'title'  => 'Contact',
+    'form' => [
+        'labels' => [
+            'name' => 'Naam',
+            'email' => 'E-mail',
+            'message' => 'Boodschap'
+        ],
+        'placeholders' => [
+
+        ],
+        'submit' => 'Verstuur',
+        'confirmation' => 'Bedankt. Uw bericht is verstuurd.'
+    ]
+];
---
+++ Resources/lang/contact/nl/contact.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title'  => 'Contact',
+    'form' => [
+        'name' => 'Naam'
+    ]
+];
