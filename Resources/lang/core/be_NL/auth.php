09854bc0e23c7ace26769329125f167b40a0153b
RienMoeyaert <rien@bitsoflove.be>
2016-01-18 10:50:57 +0100

    Update lang file for auth login

---
+++ Resources/lang/core/be_NL/auth.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'login' => [
+        'field' => [
+            'e-mail' => 'E-mailadres',
+            'password' => 'Wachtwoord'
+        ],
+        'placeholder' => [
+            'e-mail' => 'Je e-mailadres',
+            'password' => 'Je wachtwoord'
+        ],
+        'keep-logged-in' => 'Blijf aangemeld',
+        'login' => 'Aanmelden'
+    ]
+];
