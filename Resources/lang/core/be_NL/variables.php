1dec1e0373216e1b45314a7e8fa66b8de77ba175
RienMoeyaert <rien@bitsoflove.be>
2016-01-22 11:25:31 +0100

    Added variables for front to core

---
+++ Resources/lang/core/be_NL/variables.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    'tree' => [
+        'volumes' => 'Jaargangen'
+    ],
+    'alert' => [
+        'not_logged_in' => 'Niet ingelogd',
+        'message' => 'Om de inhoud van de artikelen te raadplegen dient u zich aan te melden.'
+    ],
+    'sidebar' => [
+        'search' => 'Zoek',
+        'erase_all' => 'wis alles',
+        'search_by_text' => 'Zoek op tekst',
+        'full_text' => 'Volledige tekst',
+        'full_text_placeholder' => 'bv. onderwijs',
+        'author' => 'Auteur',
+        'author_placeholder' => 'bv. Raf Verstegen',
+        'date_from' => 'Datum van',
+        'date_from_placeholder' => 'bv. 27-01-2000',
+        'date_to' => 'Datum tot',
+        'date_to_placeholder' => 'bv. 30-06-2015'
+    ],
+    'results' => [
+        'more_results' => 'Meer resultaten'
+    ],
+    'result_item' => [
+        'related_pdf' => 'Bekijk gerelateerde pdf'
+    ],
+    'facet_list' => [
+        'show_more' => 'toon meer...',
+        'titles' => [
+            'authors' => 'Auteurs',
+            'court' => 'Rechtsinstantie',
+            'place' => 'Plaats',
+            'type' => 'Type',
+            'class' => 'Bron'
+        ],
+        'refine' => 'Verfijn'
+    ]
+];
---
+++ Resources/lang/subscriber/en/validation.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'email required' => 'E-mail is required',
+    'team_name required' => 'Institution name is required',
+    'invite_members required' => 'Invite at least one user to the institution',
+    'first_name required' => 'First name is required',
+    'last_name required' => 'Last name is required'
+];
