17827572ac51ff529c2c5b124f7794f0380fadbe
RienMoeyaert <rien@bitsoflove.be>
2016-01-22 11:38:22 +0100

    Update lang keys

---
+++ Resources/lang/core/be_EN/variables.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'tree' => [
+        'volumes' => 'Volumes'
+    ],
+    'alert' => [
+        'not_logged_in' => 'Not logged in',
+        'message' => 'To see the contents of an article, you have to be logged in.'
+    ],
+    'sidebar' => [
+        'search' => 'Search',
+        'erase_all' => 'erase all',
+        'search_by_text' => 'Search by text',
+        'full_text' => 'Full text',
+        'full_text_placeholder' => 'ex. education',
+        'author' => 'Author',
+        'author_placeholder' => 'ex. Raf Verstegen',
+        'date_from' => 'Date from',
+        'date_from_placeholder' => 'ex. 27-01-2000',
+        'date_to' => 'Date to',
+        'date_to_placeholder' => 'ex. 30-06-2015'
+    ],
+    'results' => [
+        'more_results' => 'More results',
+        'no_results' => 'No results'
+    ],
+    'result_item' => [
+        'related_pdf' => 'See related pdf'
+    ],
+    'facet_list' => [
+        'show_more' => 'show more...',
+        'titles' => [
+            'authors' => 'Authos',
+            'court' => 'Legal authority',
+            'place' => 'Place',
+            'type' => 'Type',
+            'class' => 'Source'
+        ],
+        'refine' => 'Refine'
+    ]
+];
